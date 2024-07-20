a867966c8eb10c07d9d2e1ca5bb7043920e31931
PramodMarmeto <pramod.g@marmeto.com>
2024-07-20 23:12:27 +0530

    NR-first-branch || Baby Steps

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-import React from "react";
+import { createElement } from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
+const heading = createElement(
     "h1",
     {
         id: "title",
+        key: "h1"
     },
     "Heading 1"
 );
 
-const heading2 = React.createElement(
+const heading2 = createElement(
     "h2",
     {
         id: "title",
+        key: "h2"
     },
     "Heading 2"
 );
 
-const container = React.createElement(
+const container = createElement(
     "div",
     {
         id: "container",
