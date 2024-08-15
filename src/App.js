e387e93ae0ff0d8c90a2da9e0d311ca21f47e240
PramodMarmeto <pramod.g@marmeto.com>
2024-08-15 17:20:46 +0530

    Search implemented  temporarily

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const AppLayout = () => {
     </>
   );
 };
- 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //root.render(heading);
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,48 @@
 import { restaurantList } from "../config";
 import RestuarantCard from "./RestaurantCard";
+import { useState } from "react";
+
+function filterData(searchText, restaurants) {
+    return restaurants.filter((restaurant) => restaurant.info.name.includes(searchText));
+}
 
 const Body = () => {
+  // searchText is a local state variable
+  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [searchText, setSearchText] = useState(); // to create state variable
+
   return (
-    <div className="restaurant-list">
-      {restaurantList.map((restaurant) => {
-        return <RestuarantCard {...restaurant.info} key={restaurant.info.id} />;
-      })}
-      {/* <RestuarantCard {...restaurantList[0].info}/>
-        <RestuarantCard {...restaurantList[1].info}/>
-        <RestuarantCard {...restaurantList[2].info}/>
-        <RestuarantCard {...restaurantList[0].info}/>
-        <RestuarantCard {...restaurantList[1].info}/>
-        <RestuarantCard {...restaurantList[2].info}/>
-        <RestuarantCard {...restaurantList[3].info}/> */}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            // need to filter the searched el
+            const data = filterData(searchText, restaurants);
+            // update the state - restaurants
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestuarantCard {...restaurant.info} key={restaurant.info.id} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
